2edd813dcd5451405361aa06412655be3f1cf6d1
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-02 01:39:23 -0800

    Added some comments...created new firebase db and included DB-structure json file

--- src/app/api-key.ts
+++ src/app/api-key.ts
@@ -1,8 +1,17 @@
+// export var masterFirebaseConfig = {
+//   apiKey: "AIzaSyByjQfpJDslxLiq3eoqH0IOSdEn_Dd1B60",
+//   authDomain: "minesweeper-5386a.firebaseapp.com",
+//   databaseURL: "https://minesweeper-5386a.firebaseio.com",
+//   projectId: "minesweeper-5386a",
+//   storageBucket: "minesweeper-5386a.appspot.com",
+//   messagingSenderId: "604284460670"
+// }
+
 export var masterFirebaseConfig = {
-  apiKey: "AIzaSyByjQfpJDslxLiq3eoqH0IOSdEn_Dd1B60",
-  authDomain: "minesweeper-5386a.firebaseapp.com",
-  databaseURL: "https://minesweeper-5386a.firebaseio.com",
-  projectId: "minesweeper-5386a",
-  storageBucket: "minesweeper-5386a.appspot.com",
-  messagingSenderId: "604284460670"
+  apiKey: "AIzaSyDFjclpI0cGA7fusAkUQzNVXlq891DFouQ",
+  authDomain: "minesweeper-151f6.firebaseapp.com",
+  databaseURL: "https://minesweeper-151f6.firebaseio.com",
+  projectId: "minesweeper-151f6",
+  storageBucket: "minesweeper-151f6.appspot.com",
+  messagingSenderId: "1078564684296"
 }
--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -15,17 +15,23 @@ export class PlayerService {
   }
 
   getBoxById(boxId: string) {
-  return this.database.object('board/'+ boxId)
-}
+    return this.database.object('board/'+ boxId)
+  }
   updateBox(player) {
     var boxEntryInFirebase = this.getBoxById(player.$key);
     boxEntryInFirebase.update({action: player.action});
   }
 
   resetGrid() {
-    this.players.$ref.orderByKey().on("child_added", (snapshot) => {
-      // console.log(snapshot.val());
-      this.players.update(snapshot, { action: 0});
+    // $ref is just a Firebase property that represents a specific location (this.players or database.list('board')) in your DB
+    // this allows us to read/write data to that DB location
+
+    // orderByKey() just sorts (ascending) our query by $key
+    // child_added is one of the predefined Firebase event type (5 total: value, child_added, child_removed, child_changed, and child_moved)
+    this.players.$ref.orderByKey().on("child_added", (gridBox) => {
+      // gridBox is just each individual board 'child' (r0c0...r3c3)
+      this.players.update(gridBox, { action: 0 });
+      console.log(gridBox.val());
     });
   }
 }
